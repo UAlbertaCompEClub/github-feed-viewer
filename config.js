a7b53499cc85bb1f9e9d29eb95a657ab49a6cc27
Marcus Boay <marcusboay@hotmail.com>
2019-01-12 16:10:51 -0700

    remove old compe logo, add new compe and hacked logo. remove title, add compe and hacked logo in index.html. change header and commit background to rred, add left padding to compe logo, change font and add top padding to countdown in style.css

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 /**
  * Config file to set the title, deadline and Github repos for the upcoming HackEd event.
  * Properties:
- *  title: String
+ *  title: String (not in use, to use add <h1 id="event-title">HackED</h1> to header div in index.html)
  *  deadline: Date String with the following format "November 13 2017 12:00:00 GMT-0700"
  *  repos: Array of Strings that correspond to URLs of participants' Github repos.
  */ 
 var config = {
-  title: "HackEd Fall 2017",
-  deadline: "November 13 2017 12:00:00 GMT-0700",
-  repos: ["https://github.com/mabuyo/github-feed-viewer"]
+  title: "HackEd 2019",
+  deadline: "November 13 2019 12:00:00 GMT-0700",
+  repos: ["https://github.com/UAlbertaCompEClub/github-feed-viewer"]
 };
